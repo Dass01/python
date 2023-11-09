77be25de3051d43e4ce196834aedca5dd74b483c
Dass01 <149668860+Dass01@users.noreply.github.com>
2023-11-09 10:27:55 +0100

    versione1

---
+++ Es1Versione2.py
@@ -0,0 +1,5 @@
+prezzi_prodotti = (
+    ("mela",("lunedi",1.0),("martedi",1.0),("mercoledi",1.3),("giovedi",1.0),("venerdi",2.0),("sabato",1.3)),
+    ("banana",("lunedi",1.2),("martedi",1.1),("mercoledi",1.3),("giovedi",1.5),("venerdi",1.0),("sabato",1.7))
+)
+
